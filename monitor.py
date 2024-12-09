ed9cd75a0d318cc9ecd6c8cbf4db88061fa7713e
Ian Simons <iansimons93@gmail.com>
2024-12-09 10:24:37 -0500

    hacky fix for being unable to quit with ctr C

--- monitor.py
+++ monitor.py
@@ -1,14 +1,17 @@
 import os
+import time
 
 def main():
     raw_addresses = input("Welcome! please enter the IP addresses you'd like to monitor!(separate by a comma) ")
     address = raw_addresses.split(",")
     index = 0
     while True:
+        time.sleep(0.1)
         os.system("ping " + address[index] + " -c 4 > output.txt")
         with open("output.txt", "r") as infile:
             lines = infile.readlines()
         serviceDown = False
+        time.sleep(0.1)
         for i in range(len(lines)):
             if "Destination Host Unreachable" in lines[i] or "timeout" in lines[i]:
                 serviceDown = True
@@ -18,6 +21,7 @@ def main():
             exit(0)
         else:
             print("Service at "+ address[index] +" appears to be available")
+        time.sleep(0.1)
         if index < len(address)-1:
             index += 1
         else:
